e84ca1566d259269c0bc61995259b66938479aa5
Yancy <54129386+xieyunxu@users.noreply.github.com>
2023-01-07 14:05:36 +0800

    Add files via upload

---
+++ src/demo.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import _init_paths
+
+import os
+import cv2
+
+from opts import opts
+from detectors.detector_factory import detector_factory
+
+image_ext = ['jpg', 'jpeg', 'png', 'webp']
+video_ext = ['mp4', 'mov', 'avi', 'mkv']
+time_stats = ['tot', 'load', 'pre', 'net', 'dec', 'post', 'merge']
+
+def demo(opt):
+  os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
+  opt.debug = max(opt.debug, 1)
+  #self.parser.add_argument('--debug', type=int, default=0,help='level of visualization.'
+                                # '1: only show the final detection results'
+                                # '2: show the network output features'
+                                # '3: use matplot to display'  # useful when lunching training with ipython notebook
+                                # '4: save all visualizations to disk')
+  Detector = detector_factory[opt.task]
+  # self.parser.add_argument('task', default='ctdet',help='ctdet | ddd | multi_pose | exdet')
+  # detector_factory = {'exdet': ExdetDetector,'ddd': DddDetector,'ctdet': CtdetDetector,'multi_pose': MultiPoseDetector,}
+  detector = Detector(opt) # 'ctdet': CtdetDetector
+
+  if opt.demo == 'webcam' or \
+    opt.demo[opt.demo.rfind('.') + 1:].lower() in video_ext:
+    cam = cv2.VideoCapture(0 if opt.demo == 'webcam' else opt.demo)
+    detector.pause = False
+    while True:
+        _, img = cam.read()
+        cv2.imshow('input', img)
+        ret = detector.run(img)
+        time_str = ''
+        for stat in time_stats:
+          time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
+        print(time_str)
+        if cv2.waitKey(1) == 27:
+            return  # esc to quit
+  else:
+    if os.path.isdir(opt.demo):
+      image_names = []
+      ls = os.listdir(opt.demo)
+      for file_name in sorted(ls):
+          ext = file_name[file_name.rfind('.') + 1:].lower()
+          if ext in image_ext:
+              image_names.append(os.path.join(opt.demo, file_name))
+    else:
+      image_names = [opt.demo]
+      #self.parser.add_argument('--demo', default='',
+                              # help='path to image/ image folders/ video. ' 'or "webcam"')
+    
+    for (image_name) in image_names:
+      # 主体
+      ret = detector.run(image_name)
+
+      time_str = ''
+      for stat in time_stats:
+        time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
+      print(time_str)
+if __name__ == '__main__':
+    opt = opts().init()
+    demo(opt)
+    #img = cv2.imread('../images/1.JPG')
+    #print(img.shape)
+    #img = cv2.resize(img,(504,672))
+    #cv2.imwrite('../images/1_re.JPG',img)
